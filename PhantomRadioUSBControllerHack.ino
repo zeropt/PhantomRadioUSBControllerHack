e0ea320e19db6701b8fe1343b475a28f719ef025
zeropt <ferr0fluidmann@gmail.com>
2021-04-01 09:21:22 -0700

    Add Axis bounds and set them for FPV Freerider Sim

--- PhantomRadioUSBControllerHack.ino
+++ PhantomRadioUSBControllerHack.ino
@@ -16,6 +16,9 @@ Joystick_ Joystick(0x03,JOYSTICK_TYPE_GAMEPAD,
   false, false,          // No rudder or throttle
   false, false, false);  // No accelerator, brake, or steering
 
+#define AXIS_MIN -100
+#define AXIS_MAX 100
+
 //Wiring
 #define throttle_pin  A0
 #define yaw_pin       A1
@@ -37,6 +40,9 @@ Joystick_ Joystick(0x03,JOYSTICK_TYPE_GAMEPAD,
 #define roll_max      419
 #define roll_min      591
 
+#define THROW_MIN 0
+#define THROW_MAX 100
+
 //switches
 #define sa_max 1000
 #define sa_min 10
@@ -44,40 +50,40 @@ Joystick_ Joystick(0x03,JOYSTICK_TYPE_GAMEPAD,
 #define sb_min 10
 
 void setup() {
-  Joystick.setXAxisRange(-100, 100);
-  Joystick.setYAxisRange(-100, 100);
-  Joystick.setZAxisRange(-100, 100);
-  Joystick.setRxAxisRange(-100, 100);
+  Joystick.setXAxisRange(AXIS_MIN, AXIS_MAX);
+  Joystick.setYAxisRange(AXIS_MIN, AXIS_MAX);
+  Joystick.setZAxisRange(AXIS_MIN, AXIS_MAX);
+  Joystick.setRxAxisRange(AXIS_MIN, AXIS_MAX);
 
   //switches
-  Joystick.setRyAxisRange(-100, 100);
-  Joystick.setRzAxisRange(-100, 100);
+  Joystick.setRyAxisRange(AXIS_MIN, AXIS_MAX);
+  Joystick.setRzAxisRange(AXIS_MIN, AXIS_MAX);
 
   // Initialize Joystick Library
   Joystick.begin();
 }
 
 void loop() {
-  Joystick.setXAxis(constrain(map(analogRead(roll_pin), roll_min, roll_max, -100, 100), -100, 100));
-  Joystick.setYAxis(constrain(map(analogRead(pitch_pin), pitch_min, pitch_max, -100, 100), -100, 100));
-  Joystick.setZAxis(constrain(map(analogRead(throttle_pin), throttle_min, throttle_max, -100, 100), -100, 100));
-  Joystick.setRxAxis(constrain(map(analogRead(yaw_pin), yaw_min, yaw_max, -100, 100), -100, 100));
+  Joystick.setXAxis(constrain(map(analogRead(roll_pin), roll_min, roll_max, THROW_MIN, THROW_MAX), THROW_MIN, THROW_MAX));
+  Joystick.setYAxis(constrain(map(analogRead(pitch_pin), pitch_min, pitch_max, THROW_MIN, THROW_MAX), THROW_MIN, THROW_MAX));
+  Joystick.setZAxis(constrain(map(analogRead(throttle_pin), throttle_min, throttle_max, THROW_MIN, THROW_MAX), THROW_MIN, THROW_MAX));
+  Joystick.setRxAxis(constrain(map(analogRead(yaw_pin), yaw_min, yaw_max, THROW_MIN, THROW_MAX), THROW_MIN, THROW_MAX));
 
   int sa_val = analogRead(sa_pin);
   if (sa_val > sa_max) {
-    Joystick.setRyAxis(100);
+    Joystick.setRyAxis(THROW_MAX);
   } else if (sa_val < sa_min) {
-    Joystick.setRyAxis(-100);
+    Joystick.setRyAxis(THROW_MIN);
   } else {
-    Joystick.setRyAxis(0);
+    Joystick.setRyAxis((THROW_MIN + THROW_MAX)/2.0);
   }
 
   int sb_val = analogRead(sb_pin);
   if (sb_val > sb_max) {
-    Joystick.setRzAxis(100);
+    Joystick.setRzAxis(THROW_MAX);
   } else if (sb_val < sb_min) {
-    Joystick.setRzAxis(-100);
+    Joystick.setRzAxis(THROW_MIN);
   } else {
-    Joystick.setRzAxis(0);
+    Joystick.setRzAxis((THROW_MIN + THROW_MAX)/2.0);
   }
 }
